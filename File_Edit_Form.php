c083e9ccfb4ccb7c75fe9eb7d9365c8ccc3ba12c
Rhys Thompson <rhys.thompson@dcmail.ca>
2023-12-13 12:54:16 -0500

    Edited File_Edit_Form and File_Upload_Page page/ deleted uploads_pfp.php

--- File_Edit_Form.php
+++ File_Edit_Form.php
@@ -1,8 +1,8 @@
 <?php
 $files = scandir("uploads/");
-foreach ($files as $file){
+foreach ($files as $file) {
     // . looks through all files in the directory
-    if($file !== "." && $file !== ".."){
+    if ($file !== "." && $file !== "..") {
         $filePath = "uploads/" . $file;
         echo "<div class='image-box'>";
         echo "<h1>{$file}</h1>";  // Display the actual file name here
@@ -13,31 +13,32 @@ foreach ($files as $file){
             // Display the uploaded image
             echo "<img src='{$filePath}' alt='Uploaded Image'><br>";
         }
-        echo "<a href='upload_file_render.php?delete={$file}'>Delete</a>";
 
         // Edit form
-        echo "<form action='upload_file_render.php' method='post'>";
+        echo "<form action='File_Upload_Form.php' method='post'>";
         echo "<label for='newFileName'>New File Name:</label>";
         echo "<input type='text' name='newFileName' value='{$file}'>";
         echo "<input type='hidden' name='oldFileName' value='{$file}'>";
         echo "<input type='submit' name='submitEdit' value='Save'>";
         echo "</form>";
 
-        echo "<a href='upload_file_render.php?edit={$file}'>Edit</a></div><hr>";
+        echo "</div><hr>";
     }
 }
 
 // Handle form submission for editing
-if(isset($_POST['submitEdit']) && isset($_POST['newFileName']) && isset($_POST['oldFileName'])) {
+if (isset($_POST['submitEdit']) && isset($_POST['newFileName']) && isset($_POST['oldFileName'])) {
     $newFileName = $_POST['newFileName'];
     $oldFileName = $_POST['oldFileName'];
 
     $oldFilePath = "uploads/" . $oldFileName;
     $newFilePath = "uploads/" . $newFileName;
 
-    if(file_exists($oldFilePath)) {
-        if(rename($oldFilePath, $newFilePath)) {
-            echo "File name successfully updated";
+    if (file_exists($oldFilePath)) {
+        if (rename($oldFilePath, $newFilePath)) {
+            // Redirect back to File_Upload_Form.php with the updated file name
+            header("Location: File_Upload_Form.php?updatedFileName=$newFileName");
+            exit;
         } else {
             echo "Error updating file name";
         }
--- File_Upload_Form.php
+++ File_Upload_Form.php
@@ -5,6 +5,35 @@ include 'include/header.php';
 require_once 'lib/db_php.php';
 require_once 'lib/functions.php';
 
+if (!isset($_SESSION['email_address'])) {
+    // If not authenticated, redirect to the login page
+    header("Location: sign-in.php");
+    exit;
+}
+
+// Get user details from the session
+$email_address = $_SESSION['email_address'];
+$first_name = $_SESSION['first_name'];
+$last_name = $_SESSION['last_name'];
+
+echo '<div class="alert alert-success" role="alert"> Welcome ' . $first_name . ' ' . $last_name .  '</div>';
+?>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+// Handle file upload logic here
+// ...
+
+// Display the uploaded file information
+
+echo '<div class="container">';
+
+    echo '<p>File Name: ' . $_FILES["fileToUpload"]["name"] . '</p>';
+    echo '<p>File Type: ' . $_FILES["fileToUpload"]["type"] . '</p>';
+    echo '<p>File Size: ' . $_FILES["fileToUpload"]["size"] . ' bytes</p>';
+    echo '<p>Uploaded By: ' . $first_name . ' ' . $last_name . '</p>';
+    echo '</div>';
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,33 +41,108 @@ require_once 'lib/functions.php';
 <head>
     <meta charset="UTF-8">
     <title>File Upload Gallery</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        h1, label {
+            margin-bottom: 10px;
+        }
+
+        table {
+            width: 80%; /* Set the width of the table */
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
 
 
+
+    </style>
 </head>
 <body>
-    <div class="container">
-        <form action="upload_file_render.php" method="post" enctype="multipart/form-data">
-            <h1>Upload file here</h1>
-        <label for="fileToUpload">Select file to upload:</label>
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <br>
-        <input type="submit" name="submit" value="Upload File">
-        </form>
+<div class="container">
+    <div class="btn-toolbar mb-2 mb-md-0">
+
     </div>
+
+    <form action="upload_file_render.php" method="post" enctype="multipart/form-data">
+        <div>
+            <h1>Upload file here</h1>
+        </div>
+        <div>
+            <label for="fileToUpload">Select file to upload:</label>
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <br>
+            <input type="submit" name="submit" value="Upload File">
+        </div>
+    </form>
+</div>
 
 <hr>
 
-</body>
-
-<!-- Display uploaded images with a delete option. -->
 <?php
+$files = scandir("uploads/");
+if (count($files) > 2) {
+    echo '<h3>Uploaded File Information</h3>';
+    echo '<table>';
+    echo '<tr><th>File Number</th><th>File Name</th><th>Uploaded By</th><th>Action</th></tr>';
+    $fileCount = 0;
 
+    foreach ($files as $file) {
+        if ($file !== "." && $file !== "..") {
+            $fileCount++;
+            $filePath = "uploads/" . $file;
+
+            echo "<tr>";
+            echo "<td>{$fileCount}</td>";
+            echo "<td>{$file}</td>";
+            echo "<td>{$first_name} {$last_name}</td>";
+            echo "<td>";
+            echo "<a href='upload_file_render.php?delete={$file}'>Delete</a>";
+            echo " | ";
+            echo "<a href='File_Edit_Form.php?edit={$file}'>Edit</a>";
+            echo " | ";
+            echo "<a href='upload_file_render.php?move={$file}'>Move</a>";
+            echo "</td>";
+
+            echo "</tr>";
+        }
+    }
+
+    echo '</table>';
+} else {
+    echo '<p>No files uploaded yet.</p>';
+}
 ?>
+
 <?php
 // Include the footer file
 include 'include/footer.php';
 ?>
-
+</body>
 </html>
+
+
 
 
