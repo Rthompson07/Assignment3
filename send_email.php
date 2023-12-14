a46640b02d0575238f32f288a9252f20b62e8073
Rhys Thompson <rhys.thompson@dcmail.ca>
2023-12-13 22:56:49 -0500

    Updated email and send_email

--- send_email.php
+++ send_email.php
@@ -1,91 +1,77 @@
 <?php
-
-require 'vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-include_once '././config/constants.php';
-include_once '././lib/db_php.php';
+include_once 'config/constants.php';
+include_once 'lib/db_php.php';
 
-function isValidAttachment($attachment): bool
+function insertEmailData($connection, $sender, $recipient, $subject, $message): \PgSql\Result|false
 {
-    $allowedTypes = ['image/jpg', 'image/png', 'application/pdf']; // Add more allowed file types
+    $query = "INSERT INTO sent_emails (sender, recipient, subject, message) VALUES ($1, $2, $3, $4)";
 
-    return in_array($attachment['type'], $allowedTypes);
+    $result = pg_prepare($connection, "", $query);
+
+    return pg_execute($connection, "", array($sender, $recipient, $subject, $message));
 }
 
-function sendEmailViaMailTrap($sender, $recipient, $subject, $message, $attachment): bool
+function sendEmailViaMailTrap($sender, $recipient, $subject, $message): bool
 {
+    $mail = new PHPMailer(true);
     try {
-        $mail = new PHPMailer(true);
         $mail->isSMTP();
-        // Configure SMTP settings
+        $mail->Host = 'sandbox.smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Username = '4060fd5a14e424';
+        $mail->Password = '3e69569149264e';
+
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 2525;
 
         $mail->setFrom($sender);
         $mail->addAddress($recipient);
         $mail->isHTML(true);
         $mail->Subject = $subject;
+
         $mail->Body = $message;
 
-        // Handle attachments outside of this function
-
         $mail->send();
+
         return true;
-    } catch (Exception $e) {
-        echo "Email could not be sent. PHPMailer Error: {$e->getMessage()}";
+
+    }catch(Exception $e){
+        echo "Email could not be sent. PHPMailer Error: {$mail->ErrorInfo}";
         return false;
+
     }
 }
 
-function insertEmailData($connection, $sender, $recipient, $subject, $message, $attachment): \PgSql\Result|false
-{
-    $query = "INSERT INTO emails (sender, recipient, subject, message, attachment) VALUES ($1, $2, $3, $4, $5)";
-
-    return pg_execute($connection, "", array($sender, $recipient, $subject, $message, $attachment));
-}
-
-try {
+try{
     $connection = db_connect();
 
     // check to see if form is submitted
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
         $sender = filter_var($_POST['sender'], FILTER_SANITIZE_EMAIL);
         $recipient = filter_var($_POST['recipient'], FILTER_SANITIZE_EMAIL);
 
         $subject = strip_tags($_POST['subject']);
         $message = strip_tags($_POST['message']);
 
-        $attachment = $_FILES['filea'];
-
-        // Handle attachment separately
-        if (isValidAttachment($attachment)) {
-            // Check file size
-            $maxFileSize = 5000000; // 5 MB
-            if ($_FILES["filea"]["size"] > $maxFileSize) {
-                echo "Sorry, your file is too large.";
-                // You might want to exit the script or handle this error in some way.
-            } else {
-                $safeFileName = generateSafeFileName($attachment['name']);
-                $uploadPath = 'uploads/' . $safeFileName;
-                move_uploaded_file($attachment['tmp_name'], $uploadPath);
-
-                // Call sendEmailViaMailTrap here to send the email
-                if (sendEmailViaMailTrap($sender, $recipient, $subject, $message, $uploadPath)) {
-                    echo "Email sent successfully";
-                    if (insertEmailData($connection, $sender, $recipient, $subject, $message, $uploadPath)) {
-                        echo " and inserted safely into the database";
-                    } else {
-                        echo "But error storing the email in the database";
-                    }
-                } else {
-                    echo "Email failed to send";
-                }
+        if(sendEmailViaMailTrap($sender, $recipient, $subject, $message)){
+            echo "Email sent successfully";
+            if(insertEmailData($connection, $sender, $recipient, $subject, $message)){
+                echo "and inserted safely into the database";
+            }else{
+                echo "But error storing the email in the database";
             }
-        } else {
-            echo "Invalid attachment file type or size.";
+
+        }else{
+            echo "Email failed to send";
         }
     }
-} catch (Exception $e) {
+
+}catch (Exception $e){
     echo "Error: " . $e->getMessage();
+
 }
